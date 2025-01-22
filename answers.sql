df9112af56835bd0a61086717685f9b7aaefda34
cheyech <moyijulius17@gmail.com>
2025-01-22 16:48:59 +0300

    second commit added space between answers

--- answers.sql
+++ answers.sql
@@ -9,11 +9,13 @@ select customerName,email,customerAddress,TotalAmount,Status
 from customer
 left join bills
 on customer.customerID=bills.customerID;
+
 --Question 3
 select BillDate,DueDate,Status,ItemDescription,Quantity,LineTotal
 from bills
 right join
 bill_items using(BillID);
+
 --Question 4
 create table users(
 user_id int primary key auto_increment,
@@ -23,6 +25,7 @@ profile_id int primary key,
 user_id int unique,
 profile_data varchar(255),
 foreign key(user_id) references users(user_id));
+
 --Question 5
 create table departments(
 department_id int primary key auto_increment,
@@ -31,6 +34,7 @@ create table employees(
 employee_id int primary key ,
 employee_name varchar(20),
 foreign key(department_id) references departments(department_id));
+
 --Question 6
 select 
 categoryName,
